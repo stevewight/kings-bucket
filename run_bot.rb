5224ebb9ddf63b359b1ce56d7552057482aa26f9
Steve <steve@desta.me>
2012-12-18 10:44:36 -0800

    commented main script run

--- run_bot.rb
+++ run_bot.rb
@@ -4,36 +4,17 @@ require 'currency'
 require 'currency_data'
 require 'rubygems'
 require 'json'
+require 'open-uri'
+require 'open_exchange'
 
-s = Stock.new
-c = Currency.new
-c.symbol = 'USD'
-c.name = 'United States Dollar'
-c.value = nil
-c.is_base = true
-c.show
+#create the CurrencyData object passing your open exchange app id
+cd = CurrencyData.new('OPEN_EXCHANGE_APP_ID')
 
-c1 = Currency.new
-c1.symbol = 'AED'
-c1.name = 'United Arab Emirates Dirham'
-c1.value = 3.672626
-c1.is_base = false
-c1.show
+#load latest currency values
+cd.load_currency
 
-c2 = Currency.new
-c2.symbol = 'AFN'
-c2.name = 'Afghan Afghani'
-c2.value = 48.3775
-c2.is_base = false
-c2.show
+#load historic currency values by passing a date parameter
+cd.load_currency('2010-09-04')
 
-c3 = Currency.new
-c3.symbol = 'ALL'
-c3.name = 'Albanian Lek'
-c3.value = 110.223333
-c3.is_base = false
-c3.show
-
-cd = CurrencyData.new
-cd.load_all_currencies
+#show all the currencies
 cd.show
